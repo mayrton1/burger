1e5dc269e65ed3428da6a1d302ef14bf5e2a8d3b
mayrton <ayrtonmatthew@yahoo.com>
2019-11-17 21:14:19 -0700

    added files

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+var mysql = require("mysql");    
+var connection;
+
+if(process.env.JAWSDB_URL) {            
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({ // Set local connection parameters
+    host: "localhost",
+    user: "matt",
+    password: "Gator12!",
+    database: "burgers_db"
+    });
+};
+
+//creating connection
+connection.connect(function(err) {     
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+//exporting for ORM
+module.exports = connection;   
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Chesse Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Mac & Cheese Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Double Deluxe Western Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Double Triple cheese Deluxe");
